4b5c307c0dc1eb193507a3fa93e91fff8b3d25a2
aldairandre <aldairandre99@gmail.com>
2023-12-22 12:01:30 +0100

    index on mobileMenu: d7c61d4 :white_check_mark: Update

---
+++ app/components/nav/consts.ts
@@ -0,0 +1,18 @@
+const categories = [
+  ['About', '/about'],
+  ['Projects', '/projects'],
+  ['Skills', '/skills'],
+  ['Services', '/services'],
+]
+
+const categorias = [
+  ['Sobre', '/about'],
+  ['Projetos', '/projects'],
+  ['Skills', '/skills'],
+  ['Servicos', '/services'],
+]
+
+export {
+  categories,
+  categorias
+}
