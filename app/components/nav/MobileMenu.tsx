63d1fbafe285ad5b0d3c4e448cb0d68c1a751b6a
aldairandre <aldairandre99@gmail.com>
2023-12-22 13:04:33 +0100

    :lipstick: Update the UI and style files
    Mobile menu

--- app/components/nav/MobileMenu.tsx
+++ app/components/nav/MobileMenu.tsx
@@ -3,33 +3,38 @@
 import React from 'react'
 import Link from 'next/link'
 import { useMenu } from '@/app/contexts/NavBar'
-import { categories,categorias } from './consts'
+import { categories, categorias } from './consts'
 import { useLanguages } from '@/app/contexts/LanguageContext'
+import { usePathname } from 'next/navigation'
 
 const MobileMenu = () => {
 
-  const { isOn } = useMenu()
+  const pathname = usePathname()
+
+  const { isOn, changeMenu } = useMenu()
   const { language } = useLanguages()
 
   return (
     <>
       {isOn ?
         (
-          <div className='flex flex-col items-center mt-4 px-[16px] absolute top-[56px] left-0 right-0 h-screen z-[1] bg-menu-color text-white'>
+          <div className='flex flex-col items-center mt-4 px-[35px] bg-gray-300 absolute top-[56px] left-0 right-0 h-screen z-[1] bg-menu-color text-white'>
             <ul className='flex flex-col items-centertext-white w-full'>
               {
 
                 language === 'en' ?
-                (categories.map(([title, url], index) => (
-                  <li key={index} className='flex justify-between py-[13px] border-b-[1px]'>
-                    <Link href={url}>{title}</Link>
-                  </li>
-                ))) : 
-                (categorias.map(([title, url], index) => (
-                  <li key={index} className='flex justify-between py-[13px] border-b-[1px]'>
-                    <Link href={url}>{title}</Link>
-                  </li>
-                )))
+                  (categories.map(([title, url], index) => (
+                    <li key={index} className='flex justify-between py-[13px] border-b-[1px]'
+                      onClick={() => changeMenu(isOn)}
+                    >
+                      <Link href={url}>{title}</Link>
+                    </li>
+                  ))) :
+                  (categorias.map(([title, url], index) => (
+                    <li key={index} className='flex justify-between py-[13px] border-b-[1px]'>
+                      <Link href={url}>{title}</Link>
+                    </li>
+                  )))
 
               }
             </ul>
