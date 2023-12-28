8630aaea67f25e44d92dfac45a36ff270c27ab1b
aldairandre <aldairandre99@gmail.com>
2023-12-28 09:23:36 +0100

    :memo: Udate content

--- app/about/const.ts
+++ app/about/const.ts
@@ -1,5 +1,5 @@
 const dataEnglish = [
-  "My History",
+  "About Me",
   `
   Aldair Dos Santos Andre. Born on December 30, 1999, in Angola, a country in southern Africa.
   I finished high school in science in 2017, where I started to create my first scripts, and it was also at that time that I started to have contacts with my first image editing tools like Photoshop and Gimp.
@@ -9,7 +9,7 @@ const dataEnglish = [
 ]
 
 const dataPortuguese = [
-  "Minha Historia",
+  "Me Conhença",
   `Aldair Dos Santos André. Nasceu a 30 de Dezembro de 1999, natural de Angola, país do sul de África.Concluí o ensino médio em ciências em 2017, onde comecei a criar meus primeiros scripts, e foi também nessa época que comecei a ter contatos com minhas primeiras ferramentas de edição de imagem como Photoshop e Gimp.Atualmente preocupado em aprimorar minhas habilidades como Desenvolvedor Front End Web e Mobile,UI/UX Designer e Back End.
   E aberto para trabalhar em projetos que estejam trabalhando com ReactJS,React,Electron,Nextjs.
   `
