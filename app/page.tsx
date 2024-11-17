37ff18fbbf0b3e21f5e80616543832e5536faac0
Aldair Andre <nzolageral@gmail.com>
2024-11-17 15:12:30 +0100

    💄 Add component
    add line

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/hero";
+import Line from "@/components/line";
 
 export default function Home() {
   return (
     <section>
       <Hero />
+      <Line />
     </section>
   );
 }
