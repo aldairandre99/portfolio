9444557836f83e98d9dd72f5026fa3a84bd79752
Aldair Andre <nzolageral@gmail.com>
2024-11-22 12:38:36 +0100

    💄 Upade the ui
    services

--- components/services/index.tsx
+++ components/services/index.tsx
@@ -14,7 +14,7 @@ const Index = () => {
     <div className="dark:bg-[#273240] w-full mt-6 lg:mt-[240px] pt-28 pb-20">
       <DefaultLayout>
         <div className="flex flex-col lg:max-w-4xl lg:mx-auto lg:flex-row lg:justify-between ">
-          <div className="mb-8 lg:w-2/5">
+          <div className="mb-8 lg:w-2/5 lg:mb-0">
             <h1 className="text-[40px] font-bold mb-6">
               {language === "en" ? services[0] : servicos[0]}
             </h1>
@@ -22,9 +22,9 @@ const Index = () => {
               {language === "en" ? services[1] : servicos[1]}
             </p>
             <Button
-              className="hidden lg:block bg-gradient-to-l from-[#9E3DFF] to-[#F30EDC] font-poppins text-xl font-normal mt-3 lg:mt-0 lg:mr-3"
+              className="hidden lg:block w-auto bg-gradient-to-l from-[#9E3DFF] to-[#F30EDC] font-poppins text-xl font-normal mt-3 lg:mt-0 lg:mr-3"
               radius="full"
-              size="md"
+              size="lg"
             >
               {language === "en" ? services[2] : servicos[2]}
             </Button>
@@ -36,9 +36,9 @@ const Index = () => {
             <li>UI/UX</li>
           </ul>
           <Button
-            className="lg:hidden bg-gradient-to-l from-[#9E3DFF] to-[#F30EDC] font-poppins text-xl font-normal mt-6"
+            className="lg:hidden w-auto bg-gradient-to-l from-[#9E3DFF] to-[#F30EDC] font-poppins text-xl font-normal mt-3 lg:mt-0 lg:mr-3"
             radius="full"
-            size="md"
+            size="lg"
           >
             {language === "en" ? services[2] : servicos[2]}
           </Button>
