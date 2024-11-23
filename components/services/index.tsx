510c7fac71bc58fc2f27b091a373e820b6e7a03a
Aldair Andre <nzolageral@gmail.com>
2024-11-23 14:18:49 +0100

    💄 Update the ui
    button component

--- components/services/index.tsx
+++ components/services/index.tsx
@@ -24,7 +24,7 @@ const Index = () => {
             <Button
               className="hidden lg:block bg-gradient-to-l from-[#9E3DFF] to-[#F30EDC] font-poppins text-xl font-normal mt-3 lg:mt-0 lg:mr-3"
               radius="full"
-              size="md"
+              size="lg"
             >
               {language === "en" ? services[2] : servicos[2]}
             </Button>
@@ -36,9 +36,9 @@ const Index = () => {
             <li>UI/UX</li>
           </ul>
           <Button
-            className="lg:hidden bg-gradient-to-l from-[#9E3DFF] to-[#F30EDC] font-poppins text-xl font-normal mt-6"
+            className="lg:hidden bg-gradient-to-l from-[#9E3DFF] to-[#F30EDC] font-poppins text-xl font-normal mt-3 lg:mt-0 lg:mr-3"
             radius="full"
-            size="md"
+            size="lg"
           >
             {language === "en" ? services[2] : servicos[2]}
           </Button>
