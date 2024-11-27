9b78c871bd41119b76789bd445a762e7740f337d
Aldair Andre <nzolageral@gmail.com>
2024-11-27 13:52:50 +0100

    💄 Udate the ui
    line component

--- components/line/index.tsx
+++ components/line/index.tsx
@@ -1,8 +1,14 @@
+"use client"
+import { motion } from "framer-motion";
 import React from "react";
 
 const Index = () => {
   return (
-    <div className="hidden lg:block lg:w-full lg:absolute lg:top-auto lg:left-0 lg:mt-36">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ ease: 'easeIn', duration: 0.8, delay: 0.3 }}
+      className="hidden lg:block lg:w-full lg:absolute lg:top-auto lg:left-0 lg:mt-36">
       <div className="w-full h-20 bg-white absolute shadow-md -rotate-1 right-0 top-3">
         <div className="w-1/2 h-full flex justify-between items-center justify-items-center text-black mx-auto">
           <p>WEBSITES</p>
@@ -12,7 +18,7 @@ const Index = () => {
         </div>
       </div>
       <div className="w-full h-24 bg-gradient-to-l from-[#9E3DFF] to-[#F30EDC]" />
-    </div>
+    </motion.div>
   );
 };
 
